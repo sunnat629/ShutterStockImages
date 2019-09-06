01bb73975f1f7ddd781616a76e4a702bf54b048a
S M Mohi Us Sunnat <suncha629@gmail.com>
2019-09-07 01:38:24 +0600

    Master: added author details

--- app/src/main/java/dev/sunnat629/shutterstockimages/ui/MainActivity.kt
+++ app/src/main/java/dev/sunnat629/shutterstockimages/ui/MainActivity.kt
@@ -1,5 +1,14 @@
 package dev.sunnat629.shutterstockimages.ui
 
+/**
+ * <h1>ShutterStock Images</h1>
+ * This is a demo project where a user can see the image in an infinite scrollable view.
+ * For this project I have used the "Shutterstock API" for the images.
+ *
+ * @author  S M Mohi-Us Sunnat
+ * @version 1.0
+ * @since   8 September 2019
+ */
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dev.sunnat629.shutterstockimages.R
