f8689c8be0d14ecb2875a6e562fdefb91d43d62a
S M Mohi Us Sunnat <suncha629@gmail.com>
2019-09-12 19:55:05 +0600

    Rx-Flavor: import rx libs, added RxJavaCallAdapterFactory

--- app/src/main/kotlin/dev/sunnat629/shutterstockimages/ui/MainActivity.kt
+++ app/src/main/kotlin/dev/sunnat629/shutterstockimages/ui/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
--- app/src/rx_java/kotlin/dev/sunnat629/shutterstockimages/di/modules/NetworkModule.kt
+++ app/src/rx_java/kotlin/dev/sunnat629/shutterstockimages/di/modules/NetworkModule.kt
@@ -1,49 +1,95 @@
 package dev.sunnat629.shutterstockimages.di.modules
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import dagger.Module
 import dagger.Provides
-import dev.sunnat629.shutterstockimages.models.api.repositories.ImageRepository
-import dev.sunnat629.shutterstockimages.models.datasource.DataSourceFactory
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
+import dev.sunnat629.shutterstockimages.di.Authorized
+import dev.sunnat629.shutterstockimages.di.UnAuthorized
+import dev.sunnat629.shutterstockimages.models.networks.BasicAuthInterceptor
+import dev.sunnat629.shutterstockimages.models.networks.RetrofitFactory
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
 import javax.inject.Singleton
 
 /**
- * OthersModule.kt
- * This is a module class which provides CoroutineScope and DataSourceFactory during inject.
- *
- * It includes another module -
- * @see RepositoryModule for more details
+ * NetworkModule.kt
+ * This is the code module and it contains the network related providers.
  * */
-@Module(
-    includes = [
-        RepositoryModule::class]
-)
-class OthersModule {
+@Module
+class NetworkModule {
 
     /**
-     * This singleton provider provides {@linkplain CoroutineScope scope} which is a context of a scope.
+     * This singleton provider provides an OkHttpClient Builder
      * */
     @Provides
     @Singleton
-    fun provideScope(): CoroutineScope {
-        return CoroutineScope(Job() + Dispatchers.Main + Dispatchers.IO)
+    fun provideClientBuilder(): OkHttpClient.Builder {
+        return RetrofitFactory.createClientBuilder()
     }
 
     /**
-     * This singleton provider provides {@linkplain DataSourceFactory} which is responsible for
-     * retrieving the data using the DataSource and PagedList configuration.
-     *
-     * @param scope is the {@linkplain CoroutineScope scope}
-     * @param imageRepository is the {@linkplain ImageRepository repository}
+     * This singleton provider provides a {@linkplain BasicAuthInterceptor interceptor}
      * */
     @Provides
     @Singleton
-    fun provideDataSourceFactory(
-        scope: CoroutineScope,
-        imageRepository: ImageRepository
-    ): DataSourceFactory {
-        return DataSourceFactory(scope, imageRepository)
+    fun provideBasicAuthInterceptor(): BasicAuthInterceptor {
+        return BasicAuthInterceptor()
+    }
+
+    /**
+     * This singleton provider provides an authorized OkHttpClient
+     *
+     * @param clientBuilder is an {@code @Authorized} OkHttpClient Builder
+     * @param basicAuthInterceptor is a {@linkplain BasicAuthInterceptor interceptor}
+     * */
+    @Provides
+    @Singleton
+    @Authorized
+    fun provideAuthOkHttpClient(
+        clientBuilder: OkHttpClient.Builder,
+        basicAuthInterceptor: BasicAuthInterceptor
+    ): OkHttpClient {
+        return clientBuilder
+            .addInterceptor(basicAuthInterceptor)
+            .build()
+    }
+
+    /**
+     * This singleton provider provides an authorized Retrofit
+     *
+     * @param okHttpClient is an {@code @Authorized} OkHttpClient
+     * */
+    @Provides
+    @Singleton
+    @Authorized
+    fun provideAuthRetrofit(@Authorized okHttpClient: OkHttpClient): Retrofit {
+        return RetrofitFactory.createRetrofit(okHttpClient, RxJavaCallAdapterFactory.create())
+    }
+
+    /**
+     * This singleton provider provides an unauthorized OkHttpClient
+     *
+     * @param clientBuilder is an {@code @UnAuthorized} OkHttpClient Builder
+     * */
+    @Provides
+    @Singleton
+    @UnAuthorized
+    fun provideOkHttpClient(
+        clientBuilder: OkHttpClient.Builder
+    ): OkHttpClient {
+        return clientBuilder
+            .build()
+    }
+
+    /**
+     * This singleton provider provides an unauthorized Retrofit
+     *
+     * @param okHttpClient is an {@code @UnAuthorized} OkHttpClient
+     * */
+    @Provides
+    @Singleton
+    @UnAuthorized
+    fun provideRetrofit(@UnAuthorized okHttpClient: OkHttpClient): Retrofit {
+        return RetrofitFactory.createRetrofit(okHttpClient, RxJavaCallAdapterFactory.create())
     }
 }
